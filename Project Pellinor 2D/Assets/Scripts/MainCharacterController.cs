a3b85eb6ea929702b964610fa292bb1fec836d08
djuhascik837 <djuhascik837@users.noreply.github.com>
2020-05-02 19:24:14 +0100

    Simple Movement of Character

---
+++ Project Pellinor 2D/Assets/Scripts/MainCharacterController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCharacterController : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D rigidbody2D;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D> ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector2 position = rigidbody2D.position;
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+        position.y = position.y + speed * vertical * Time.deltaTime;
+
+        rigidbody2D.MovePosition(position);
+        
+
+    }
+}
