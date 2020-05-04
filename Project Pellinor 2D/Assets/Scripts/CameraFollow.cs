bb0f8a5e5ac0b378474f45a9535f80e0b4335540
djuhascik837 <djuhascik837@users.noreply.github.com>
2020-05-04 15:43:29 +0100

    Camera Following Player
    Camera Jittering happening when following needs to be looked at further

---
+++ Project Pellinor 2D/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public Transform target;
+    public float cameraDistance = 10.0f;
+
+    void Awake() 
+    {
+        GetComponent<UnityEngine.Camera>().orthographicSize = ((Screen.height / 2 ) / cameraDistance);
+    }
+
+    void LateUpdate() 
+    {
+        transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);
+    }
+
+}
