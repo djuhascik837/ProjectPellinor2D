40d0556c14878c9aef21aee98d8c7830a15ec8c4
djuhascik837 <djuhascik837@users.noreply.github.com>
2020-05-04 18:49:10 +0100

    Cammera changes

--- Project Pellinor 2D/Assets/Scripts/CameraFollow.cs
+++ Project Pellinor 2D/Assets/Scripts/CameraFollow.cs
@@ -1,18 +1,50 @@
 ﻿using UnityEngine;
 
 public class CameraFollow : MonoBehaviour {
-
     public Transform target;
-    public float cameraDistance = 10.0f;
-
-    void Awake() 
-    {
-        GetComponent<UnityEngine.Camera>().orthographicSize = ((Screen.height / 2 ) / cameraDistance);
-    }
+    //public float cameraDistance = 10.0f;
+    public float boundX = 2.0f;
+    public float boundY = 1.5f; 
+    // void Awake() 
+    // {
+    //     GetComponent<UnityEngine.Camera>().orthographicSize = ((Screen.height / 2 ) / cameraDistance);
+    // }
 
     void LateUpdate() 
     {
-        transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);
+        //transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);
+        Vector3 delta = Vector3.zero;
+        float dx = target.position.x - transform.position.x;
+
+        //x axis
+        if (dx > boundX || dx < -boundX)
+        {
+            if(transform.position.x < target.position.x)
+            {
+                delta.x = dx - boundX;
+            }
+            else
+            {
+                delta.x = dx + boundX;
+            }
+        }
+
+        //y axis
+        float dy = target.position.y - transform.position.y;
+        if (dy > boundY || dy < -boundY)
+        {
+            if(transform.position.y < target.position.y)
+            {
+                delta.y = dy - boundY;
+            }
+            else
+            {
+                delta.y = dy + boundY;
+            }
+        }
+
+        //move camera
+        transform.position += transform.position + delta;
     }
 
 }
